bd17bdb5b758bcede206045ede8de8db2033798c
caiogmello <caiogomedemello@gmail.com>
2024-11-08 16:21:11 -0300

    exception handler

---
+++ src/exception_handler.py
@@ -0,0 +1,8 @@
+class ExceptionHandler(Exception):
+    def __init__(self, message):
+        self._message = f"Request failed: {message}"
+        super().__init__(message)
+        
+
+    def __str__(self):
+        return self._message
